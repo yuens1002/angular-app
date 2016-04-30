b277509d8be7c36bcd0a5733190088d97264ff60
Sunny Yuen <yuens1002@gmail.com>
2016-04-30 13:15:37 -0400

    initial angular-app project commits

---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularAppApp
+ */
+angular.module('angularAppApp')
+  .controller('ContactCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
